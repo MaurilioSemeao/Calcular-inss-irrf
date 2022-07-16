e17e8a23a10686c947ba9850c3578c5d63eac2da
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-07-16 18:21:51 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace calcuo_inss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntCalcular.Location = new System.Drawing.Point(12, 285);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(143, 38);
+            this.bntCalcular.TabIndex = 0;
+            this.bntCalcular.Text = "Calcular";
+            this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 74);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(143, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(12, 151);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(143, 20);
+            this.txtSalario.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Salario";
+            // 
+            // txtLista
+            // 
+            this.txtLista.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLista.Location = new System.Drawing.Point(181, 57);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(375, 289);
+            this.txtLista.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 415);
+            this.Controls.Add(this.txtLista);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.bntCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntCalcular;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLista;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcuo_inss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+            double salario = 0, salarioDesc = 0, descInss =0, descIrf = 0;
+            string nome = "", lista = "";
+
+            salario = Convert.ToDouble(txtSalario.Text);
+            nome = txtNome.Text;
+
+            if(salario <= 1212)
+            {
+                descInss = salario * 0.075;
+            
+            }else if(salario <= 2427.35)
+            {
+                descInss = salario * 0.09;
+            
+            }else if(salario <= 3641.03)
+            {
+                descInss = salario * 0.12;
+            
+            }else if(salario <= 7082.22)
+            { 
+                descInss = salario * 0.14;
+            }
+            else
+            {
+                descInss = 7082.22 * 0.14;
+            }
+
+            salarioDesc = salario - descInss;
+
+            if(salarioDesc <= 1903.98)
+            {
+                descIrf = 0;
+
+            }else if(salarioDesc <= 2826.65)
+            {
+                descIrf = (salario - descInss)*0.075;
+   
+            }else if(salarioDesc <= 3751.05)
+            {
+                descIrf = (salario - descInss) * 0.15;
+            }
+            else if(salarioDesc <= 4664.68)
+            {
+                descIrf = (salario - descInss) * 0.225;
+            }
+            else
+            {
+                descIrf = (salario - descInss) * 0.227;
+            }
+
+
+            lista = "Folha de Pagamento " + Environment.NewLine;
+            lista += "------------------------------------------------------"+ Environment.NewLine;
+            lista += "Funcionario: "+ nome + Environment.NewLine;
+            lista += "Salario Bruto: "+ salario + Environment.NewLine;
+            lista += "Desconto:" + Environment.NewLine;
+            lista += "      INSS :" + descInss + Environment.NewLine;
+            lista += "       IRF :" + descIrf + Environment.NewLine;
+            lista += "Salario Liquido: " +( salario - descInss -descIrf ) + Environment.NewLine;
+            lista += "------------------------------------------------------";
+
+            txtLista.Text = lista;
+
+            
+             
+            
+
+
+
+        }
+    }
+}
